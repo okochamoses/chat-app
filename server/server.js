937585440c2ad428b72388864b2f2b27e05654a8
Okocha Moses <okocha.chukwutem@gmail.com>
2018-05-07 13:53:27 +0100

    New user broadcast alert added

--- server/server.js
+++ server/server.js
@@ -13,12 +13,23 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
-            date: new Date().getTime()
+            createdAt: new Date().getTime()
         });
     });
 
